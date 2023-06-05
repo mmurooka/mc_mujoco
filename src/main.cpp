f824deb77466312bc2806b79d94b71022b5dcde7
Masaki Murooka <m-murooka@aist.go.jp>
2023-06-05 18:36:21 +0900

    Load Plugin libraries.
    Copied from https://github.com/deepmind/mujoco/blob/main/simulate/main.cc

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,164 @@ namespace po = boost::program_options;
 
 bool render_state = true;
 
+#define MUJOCO_PLUGIN_DIR "mujoco_plugin"
+
+// return the path to the directory containing the current executable
+// used to determine the location of auto-loaded plugin libraries
+std::string getExecutableDir()
+{
+#if defined(_WIN32) || defined(__CYGWIN__)
+  constexpr char kPathSep = '\\';
+  std::string realpath = [&]() -> std::string {
+    std::unique_ptr<char[]> realpath(nullptr);
+    DWORD buf_size = 128;
+    bool success = false;
+    while(!success)
+    {
+      realpath.reset(new(std::nothrow) char[buf_size]);
+      if(!realpath)
+      {
+        std::cerr << "cannot allocate memory to store executable path\n";
+        return "";
+      }
+
+      DWORD written = GetModuleFileNameA(nullptr, realpath.get(), buf_size);
+      if(written < buf_size)
+      {
+        success = true;
+      }
+      else if(written == buf_size)
+      {
+        // realpath is too small, grow and retry
+        buf_size *= 2;
+      }
+      else
+      {
+        std::cerr << "failed to retrieve executable path: " << GetLastError() << "\n";
+        return "";
+      }
+    }
+    return realpath.get();
+  }();
+#else
+  constexpr char kPathSep = '/';
+#  if defined(__APPLE__)
+  std::unique_ptr<char[]> buf(nullptr);
+  {
+    std::uint32_t buf_size = 0;
+    _NSGetExecutablePath(nullptr, &buf_size);
+    buf.reset(new char[buf_size]);
+    if(!buf)
+    {
+      std::cerr << "cannot allocate memory to store executable path\n";
+      return "";
+    }
+    if(_NSGetExecutablePath(buf.get(), &buf_size))
+    {
+      std::cerr << "unexpected error from _NSGetExecutablePath\n";
+    }
+  }
+  const char * path = buf.get();
+#  else
+  const char * path = "/proc/self/exe";
+#  endif
+  std::string realpath = [&]() -> std::string {
+    std::unique_ptr<char[]> realpath(nullptr);
+    std::uint32_t buf_size = 128;
+    bool success = false;
+    while(!success)
+    {
+      realpath.reset(new(std::nothrow) char[buf_size]);
+      if(!realpath)
+      {
+        std::cerr << "cannot allocate memory to store executable path\n";
+        return "";
+      }
+
+      std::size_t written = readlink(path, realpath.get(), buf_size);
+      if(written < buf_size)
+      {
+        realpath.get()[written] = '\0';
+        success = true;
+      }
+      else if(written == -1)
+      {
+        if(errno == EINVAL)
+        {
+          // path is already not a symlink, just use it
+          return path;
+        }
+
+        std::cerr << "error while resolving executable path: " << strerror(errno) << '\n';
+        return "";
+      }
+      else
+      {
+        // realpath is too small, grow and retry
+        buf_size *= 2;
+      }
+    }
+    return realpath.get();
+  }();
+#endif
+
+  if(realpath.empty())
+  {
+    return "";
+  }
+
+  for(std::size_t i = realpath.size() - 1; i > 0; --i)
+  {
+    if(realpath.c_str()[i] == kPathSep)
+    {
+      return realpath.substr(0, i);
+    }
+  }
+
+  // don't scan through the entire file system's root
+  return "";
+}
+
+// scan for libraries in the plugin directory to load additional plugins
+void scanPluginLibraries()
+{
+  // check and print plugins that are linked directly into the executable
+  int nplugin = mjp_pluginCount();
+  if(nplugin)
+  {
+    std::printf("Built-in plugins:\n");
+    for(int i = 0; i < nplugin; ++i)
+    {
+      std::printf("    %s\n", mjp_getPluginAtSlot(i)->name);
+    }
+  }
+
+  // define platform-specific strings
+#if defined(_WIN32) || defined(__CYGWIN__)
+  const std::string sep = "\\";
+#else
+  const std::string sep = "/";
+#endif
+
+  // try to open the ${EXECDIR}/plugin directory
+  // ${EXECDIR} is the directory containing the simulate binary itself
+  const std::string executable_dir = getExecutableDir();
+  if(executable_dir.empty())
+  {
+    return;
+  }
+
+  const std::string plugin_dir = getExecutableDir() + sep + MUJOCO_PLUGIN_DIR;
+  mj_loadAllPluginLibraries(
+      plugin_dir.c_str(), +[](const char * filename, int first, int count) {
+        std::printf("Plugins registered by library '%s':\n", filename);
+        for(int i = first; i < first + count; ++i)
+        {
+          std::printf("    %s\n", mjp_getPluginAtSlot(i)->name);
+        }
+      });
+}
+
 void simulate(mc_mujoco::MjSim & mj_sim)
 {
   bool done = false;
@@ -68,6 +226,9 @@ int main(int argc, char * argv[])
       config.visualize_collisions = vm["with-collisions"].as<bool>();
     }
   }
+
+  scanPluginLibraries();
+
   mc_mujoco::MjSim mj_sim(config);
 
   std::thread simThread(simulate, std::ref(mj_sim));
